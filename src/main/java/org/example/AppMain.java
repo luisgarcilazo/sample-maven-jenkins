281b428159a54e3e2fa8558d0bef4cbcfc41c1cb
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2023-06-06 12:39:45 -0500

    checking

---
+++ src/main/java/org/example/AppMain.java
@@ -0,0 +1,10 @@
+package org.example;
+//help from https://www.sohamkamani.com/java/cli-app-with-maven/,
+// https://www.softwaretestinghelp.com/maven-with-jenkins/
+// and
+// https://www.javatpoint.com/no-main-manifest-attribute
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world! from maven");
+    }
+}
